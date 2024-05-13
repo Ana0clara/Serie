cbc0a8f0730cbabf537bc29c9126ceb604b4ad45
Ana clara Barbosa <169068132+Ana0clara@users.noreply.github.com>
2024-05-13 17:03:33 -0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,21 @@
-
-
 public class Main {
   public static void main(String[] args) {
-    System.out.println("—————");
-    System.out.println("Serie"); 
-    System.out.println("—————");
+    System.out.println("     ———————————————————");
+    System.out.println("     the big bang theory"); 
+    System.out.println("     ———————————————————");
+    //procurar uma serie
+    System.out.println(" "); 
+    System.out.println("-----------------"); 
+
+    System.out.println("+ Sobre a serie +"); 
+// so o titulo-
+    System.out.println("-----------------"); 
+
+
     System.out.println(" "); 
 
-    System.out.println("- Sobre a serie -"); 
-
-    System.out.println(" "); 
-
-    System.out.println("+ texto texto texto"); 
-
+    System.out.println("- Os melhores amigos e companheiros de apartamento Leonard e Sheldon são físicos que trabalham no Instituto Tecnológico da Califórnia (Caltech) e são capazes de explicar a todo mundo mais do que gostariam de saber sobre física quântica, mas superar as situações sociais mais básicas, especialmente aquelas envolvendo mulheres, os deixa completamente perdidos. Que sorte quando a gostosa garçonete/atriz aspirante Penny se muda para o apartamento ao lado. Wolowitz e Koothrappali, também da Caltech, são vistos frequentemente passando o tempo com Leonard e Sheldon. Será que os mundos vão se colidir? Será que Einstein teorizava na floresta?"); 
+// sinopse da serie 
     System.out.println(" "); 
     System.out.println("---------------- "); 
 
@@ -21,8 +24,8 @@ public class Main {
 
     System.out.println(" "); 
 
-    System.out.println("link"); 
-    
+    System.out.println("https://www.youtube.com/watch?v=WBb3fojgW0Q"); 
+    //lembrar de por o link
     System.out.println(" "); 
 
     System.out.println("----------------"); 
