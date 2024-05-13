8ae71f361f052d627db4837a29404afa1509607b
Ana clara Barbosa <169068132+Ana0clara@users.noreply.github.com>
2024-05-13 14:51:53 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,36 @@
+
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("—————");
+    System.out.println("Serie"); 
+    System.out.println("—————");
+    System.out.println(" "); 
+
+    System.out.println("- Sobre a serie -"); 
+
+    System.out.println(" "); 
+
+    System.out.println("+ texto texto texto"); 
+
+    System.out.println(" "); 
+    System.out.println("---------------- "); 
+
+
+    System.out.println("Voce pode ver o traile em:"); 
+
+    System.out.println(" "); 
+
+    System.out.println("link"); 
+    
+    System.out.println(" "); 
+
+    System.out.println("----------------"); 
+
+
+
+
+
+  }
+
+}
